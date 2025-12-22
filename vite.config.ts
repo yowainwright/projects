1abeb570004d9548182040f641b9c87382380f2f
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-12-21 23:05:43 -0800

    chore: updates favicons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/projects/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
