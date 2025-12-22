c1b690f837a923e8e3c33e10a9a61308db7bf6b9
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-12-22 04:30:45 -0800

    chore: updates config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routes';
 import './index.css';
 
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, basepath: '/projects' });
 
 declare module '@tanstack/react-router' {
   interface Register {
